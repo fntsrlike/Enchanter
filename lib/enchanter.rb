fbc0494ecb31be47f2f1c9d3f5251f7cf880496e
Roushi Lin <fntsrlike@gmail.com>
2018-04-28 23:29:49 +0800

    [Coding Style] Apply Rubocop styles

--- lib/enchanter.rb
+++ lib/enchanter.rb
@@ -1,5 +1,8 @@
-require "enchanter/version"
+# frozen_string_literal: true
 
+require 'enchanter/version'
+
+# The top level Dotenv module. The entrypoint for the application logic.
 module Enchanter
   # Your code goes here...
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,6 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "enchanter"
+# frozen_string_literal: true
 
-require "minitest/autorun"
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'enchanter'
+
+require 'minitest/autorun'
