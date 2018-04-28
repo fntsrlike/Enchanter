43e417161c3dac217671b229fd4170cd66f8efa8
Roushi Lin <fntsrlike@gmail.com>
2018-04-29 01:37:33 +0800

    [Emerald] Add testing of name method

---
+++ lib/enchanter/emerald.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Enchanter
+  # To verify plugin files
+  module Emerald
+    module_function
+
+    def name(filename)
+      # TODO
+    end
+  end
+end
---
+++ test/emerald/name_test.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'enchanter/emerald'
+
+module Enchanter
+  class EmeraldTest < Minitest::Test
+    def test_with_big_camelcase
+      assert_equal 'CommandBlock', Emerald.name('CommandBlock-1.12.2.jar')
+    end
+
+    def test_with_small_camelcase
+      assert_equal 'commandBlock', Emerald.name('commandBlock-1.12.2.jar')
+    end
+
+    def test_with_underscore
+      assert_equal 'command_block', Emerald.name('command_block-1.12.2.jar')
+    end
+
+    def test_with_snakecase
+      assert_equal 'command', Emerald.name('command-block-1.12.2.jar')
+    end
+
+    def test_with_alphanumeric
+      assert_equal 'CommandBlock2018',
+                   Emerald.name('CommandBlock2018-1.12.2.jar')
+    end
+
+    def test_without_version
+      assert_equal 'CommandBlock', Emerald.name('CommandBlock.jar')
+    end
+
+    def test_without_name
+      assert_equal '', Emerald.name('-1.12.2.jar')
+    end
+  end
+end
